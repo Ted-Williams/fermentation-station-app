2d1bfb02c1f00f25bcd10601e0d856648fe1fef7
Ted-Williams <ted_Williams@hotmail.co.uk>
2022-03-29 22:11:34 +0000

    added remove and update to basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -21,3 +21,18 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+    
+
+def adjust_basket(request, item_id):
+    """ Change a quantity of the specified product to the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
