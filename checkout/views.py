8e990f9e7ad9b6c99304fa98546c6b6e1b7f17e1
Ted-Williams <ted_Williams@hotmail.co.uk>
2022-03-31 18:26:27 +0000

    corrected errors on stripe checkout

--- checkout/views.py
+++ checkout/views.py
@@ -35,7 +35,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': client_secret,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
