0c006a34c42ff89e3abfe510cdb0ef3364c73377
Ted-Williams <ted_Williams@hotmail.co.uk>
2022-03-31 22:07:15 +0000

    INsert

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from basket.contexts import basket_contents
+from .models import Order, OrderLineItem
+from products.models import Product
+from bag.contexts import bag_contents
 
 import stripe
 
