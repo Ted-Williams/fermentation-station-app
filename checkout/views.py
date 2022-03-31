965078a89433d5d330a6ddd64b94a09f89bee266
Ted-Williams <ted_Williams@hotmail.co.uk>
2022-03-31 22:09:26 +0000

    Small changes

--- checkout/views.py
+++ checkout/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from bag.contexts import bag_contents
+from basket.contexts import basket_contents
 
 import stripe
 
